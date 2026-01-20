0bb3ae6e9331b0fab1aeda285b7f0a83cedb3e20
Lexior <aleksykb@gmail.com>
2026-01-20 11:29:26 +0100

    work

--- CombatSharpedSem1/Program.cs
+++ CombatSharpedSem1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System;
+
+public class Program
+{
+    // GAME SETUP
+    Random rng = new Random();
+    // Store which subsequent encounter the player is on
+    int nEncounter = 0;
+    // Initialize the player
+
+
+    static void Main()
+    {
+        
+        Console.WriteLine("---|| Combat Sharped ||---");
+        Console.WriteLine("Welcome. In this game you'll face endless waves of enemies.\nAfter defeating an enemy, you'll gain XP which would eventually lead you to get to higher levels,\nunlocking extra skills.");
+        Console.WriteLine("Just tell me your name first, and lets get started!");
+        Player player = new Player(Console.ReadLine());
+        Console.WriteLine($"Nice to meet you, {PlayerName}!\nGood luck and have fun!");
+    }
+
+    
+}
+
+public class Player
+{
+    public string Name;
+    public int Level;
+    public int XP;
+    public int MaxHP;
+    public int CurrentHP;
+    public int AttackPower;
+    public List<string> Skills;
+
+    public Player(string name)
+    {
+        Name = name;
+        Level = 1;
+        XP = 0;
+        MaxHP = 100;
+        CurrentHP = MaxHP;
+        AttackPower = 12;
+        Skills = new List<string> { "Slash", "Block" };
+    }
+}
--- CombatSharpedSem1/obj/Debug/net9.0/CombatSharpedSem1.AssemblyInfo.cs
+++ CombatSharpedSem1/obj/Debug/net9.0/CombatSharpedSem1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CombatSharpedSem1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea78f8f42038851c384eaa11dcb0b24f11c80de")]
 [assembly: System.Reflection.AssemblyProductAttribute("CombatSharpedSem1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CombatSharpedSem1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
